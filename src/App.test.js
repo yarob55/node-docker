6837b47c2e50a6bcb123282093c3247316e511ec
Yarob Al Mostafa <yarob.hm@gmail.com>
2019-08-03 20:48:24 +0300

    dklsf

--- src/App.test.js
+++ src/App.test.js
@@ -14,4 +14,23 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+
+// deploy:
+//   provider: elasticbeanstalk
+//   region: "us-east-2"
+//   app: "node-docker"
+//   env: "NodeDocker-env"
+//   bucket_name: "elasticbeanstalk-us-east-2-925342842908"
+//   bucket_path: "node-docker"
+//   on:
+//     branch: master
+//   access_key_id: $AWS_ACCESS_KEY
+//   secret_access_key:
+//     secret: "$AWS_SECRET_KEY"
 
